c8212c3443d3dfe941ca98f141f8e96d659b93fd
jay-eon <68725355+jay-eon@users.noreply.github.com>
2023-05-28 00:49:10 +0900

    Update delete_data_sql.py

--- sql/delete_data_sql.py
+++ sql/delete_data_sql.py
@@ -1,7 +1,13 @@
-def generate_delete_query(project_id, dataset, table_nm, execute_date, **context):
+def delete_mart_data(project_id, dataset, table_nm, execute_date, **context):
     sql = """
         DELETE FROM `{0}.{1}.{2}`
         WHERE event_date = DATE('{3}')
     """.format(project_id, dataset, table_nm, execute_date)
     
-    return sql
+    return BigQueryOperator(
+        task_id='delete_mart_data',
+        sql=generate_delete_query,
+        use_legacy_sql=False,
+        bigquery_conn_id='bigquery_conn',
+        dag=dag
+    )
