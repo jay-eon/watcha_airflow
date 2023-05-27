7bbd1dfade1c9809aa3d762195470d82a935b11a
jay-eon <68725355+jay-eon@users.noreply.github.com>
2023-05-28 01:03:34 +0900

    Update delete_data_sql.py

--- sql/delete_data_sql.py
+++ sql/delete_data_sql.py
@@ -1,16 +1,7 @@
-from airflow.contrib.operators.bigquery_operator import BigQueryOperator
-from airflow.utils.trigger_rule import TriggerRule
-
 def delete_mart_data(project_id, dataset, table_nm, execute_date, **context):
     delete_sql = """
         DELETE FROM `{0}.{1}.{2}`
         WHERE event_date = DATE('{3}')
     """.format(project_id, dataset, table_nm, execute_date)
     
-    return BigQueryOperator(
-        task_id='delete_mart_data',
-        sql=delete_sql,
-        use_legacy_sql=False,
-        bigquery_conn_id='bigquery_conn',
-        dag=dag
-    )
+    return delete_sql
